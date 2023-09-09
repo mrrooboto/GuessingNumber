dc952774f15348ee8d39377e86f727ed5a432029
mrrooboto <107651477+mrrooboto@users.noreply.github.com>
2023-09-09 22:58:59 +0200

    GuessingNumbers
    Version 0.1

--- GuessingNumbers/src/com/benjamin/MainProgram.java
+++ GuessingNumbers/src/com/benjamin/MainProgram.java
@@ -1,9 +1,26 @@
 package com.benjamin;
+import java.util.Scanner;
 
 public class MainProgram {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		//create a random number for the user to guess
 		int theNumber = (int)(Math.random() * 100 + 1);
-		System.out.println(theNumber);
+		int guess = 0;
+		while (guess != theNumber) {
+			System.out.println("Guess a number between 1 and 100:");
+			guess = sc.nextInt();
+			System.out.println("You entered " + guess + ".");
+			if (guess > theNumber) {
+				System.out.println(guess + " is too low. Try again.");
+			} else if (guess < theNumber) {
+				System.out.println(guess + " is too high. Try again.");
+			} else {
+				System.out.println(guess + " is correct. You win!");
+			} 
+		}// end of the loop 
+		
+		
+		
 	}
 }
