39e4cdffda63c488fe47d16bf2e1a5f71aeefedb
mrrooboto <107651477+mrrooboto@users.noreply.github.com>
2023-09-10 15:24:02 +0200

    Version 0.3
    Finished game skeleton, next up is GUI.

--- GuessingNumbers/src/com/benjamin/MainProgram.java
+++ GuessingNumbers/src/com/benjamin/MainProgram.java
@@ -4,23 +4,28 @@ import java.util.Scanner;
 public class MainProgram {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//create a random number for the user to guess
-		int theNumber = (int)(Math.random() * 100 + 1);
-		int guess = 0;
-		while (guess != theNumber) {
-			System.out.println("Guess a number between 1 and 100:");
-			guess = sc.nextInt();
-			System.out.println("You entered " + guess + ".");
-			if (guess > theNumber) {
-				System.out.println(guess + " is too low. Try again.");
-			} else if (guess < theNumber) {
-				System.out.println(guess + " is too high. Try again.");
-			} else {
-				System.out.println(guess + " is correct. You win!");
-			} 
-		}// end of the loop 
-		
-		
+		String playAgain = "";
+		do {
+			//create a random number for the user to guess
+			int theNumber = (int)(Math.random() * 100 + 1);
+			int guess = 0;
+			while (guess != theNumber) {
+				System.out.println("Guess a number between 1 and 100:");
+				guess = sc.nextInt();
+				System.out.println("You entered " + guess + ".");
+				if (guess < theNumber) {
+					System.out.println(guess + " is too low. Try again.");
+				} else if (guess > theNumber) {
+					System.out.println(guess + " is too high. Try again.");
+				} else {
+					System.out.println(guess + " is correct. You win!");
+				} 
+			}	// end of the loop 
+			System.out.println("Would you like to play again ? (y/n)?");
+			playAgain = sc.next();
+		} while (playAgain.equalsIgnoreCase("y"));
+		System.out.println("Thank you for playing! Goodbye.");
+		sc.close();
 		
 	}
 }
